49840fc1d62d7bb87b28a5a3a8b4a92899f493a0
AMao7 <abdimalikmao7@gmail.com>
2020-02-07 11:27:37 +0000

    work in progress for fizzbuzz

---
+++ Fizz_Buzz_Functions.py
@@ -0,0 +1,13 @@
+def fizz(x):
+    if x % 5 == 0:
+        return "fizz"
+
+
+def buzz(x):
+    if x % 3 == 0:
+        return "buzz"
+
+
+def fizz_buzz(x):
+    if x % 15 == 0:
+        return "fizzbuzz"
---
+++ README.md
@@ -0,0 +1,5 @@
+# Project 4: FizzBuzz Functions
+
+### To automate Project 2 with the use of functions
+
+#### Utilised: Functions
